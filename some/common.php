88363a90801583d8e4001decf622f7bf3dd7c077
loveling <1982404464@qq.com>
2019-03-27 11:28:32 +0700

    Update common.php

--- some/common.php
+++ some/common.php
@@ -492,3 +492,32 @@ function make_dir($dir, $permission = 0777)
 
 	return @mkdir($dir, $permission);
 }
+$array1 = array(
+            0=>array('id'=>8,'name'=>'Apple','age'=> 18),
+            1=>array('id'=>8,'name'=>'Bed','age'=>17),
+            2=>array('id'=>5,'name'=>'Cos','age'=>16),
+            3=>array('id'=>5,'name'=>'Cos','age'=>14)
+        );
+function sortArrByManyField(){
+    $args = func_get_args(); // 获取函数的参数的数组
+    if(empty($args)){
+	return null;
+    }
+    $arr = array_shift($args);
+    if(!is_array($arr)){
+	throw new Exception("第一个参数不为数组");
+    }
+    foreach($args as $key => $field){
+	if(is_string($field)){
+	    $temp = array();
+	    foreach($arr as $index=> $val){
+		$temp[$index] = $val[$field];
+	    }
+	    $args[$key] = $temp;
+	}
+    }
+    $args[] = &$arr;//引用值
+    call_user_func_array('array_multisort',$args);
+    return array_pop($args);
+}
+$arr = sortArrByManyField($array1,'id',SORT_ASC,'name',SORT_ASC,'age',SORT_DESC);
